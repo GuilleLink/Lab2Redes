deae9e2113bd344b21f0021b983ae2dcf0932a15
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2020-08-01 20:54:53 -0600

    Lab2 initialize

---
+++ lab2.py
@@ -0,0 +1,27 @@
+#Universidad del Valle de Guatemala
+#Redes
+#Juan Guillermo Sandoval Lacayo - 17577
+#Otto Alexander Trujillo Contreras - 17189
+#Laboratorio 2
+
+
+
+#Importacion de librerias
+from bitarray import bitarray
+
+
+SentMessage = bitarray()
+
+#Hola en ASCII es:
+#0100 1000    0110 1111    0110 1100    0110 0001
+#Todo junto
+#01001000011011110110110001100001
+
+#parte 1 
+
+MessageA = input("Ingrese un texto a enviar")
+
+MessageABinary = bitarray(MessageA)
+
+print(MessageABinary)
+print("HOLAA")
