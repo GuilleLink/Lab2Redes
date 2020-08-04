3a639a11a4d103af6f1057e5458e8d5d3945e0d3
tru17189 <32879914+tru17189@users.noreply.github.com>
2020-08-04 00:54:59 -0600

    Algoritmo de hamming

--- lab2.py
+++ lab2.py
@@ -26,24 +26,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 print('Received', repr(data))
 
 '''
-#parte 2
-def hamming(mensaje):
-	mensaje = list(mensaje)
-
-	x = posicion = 0	
-	while posicion < len(mensaje):
-		posicion = 2 ** x
-		# insertamos el valor de paridad
-		if posicion < len(mensaje):
-			mensaje.insert(posicion-1, "*")
-		else:
-			break
-		x += 1
-
-	cadena = "".join(cadena)
-	return cadena
-
-#parte 1
+#parte 1 - STRING -> BINARY -> BITARRAY // Buscar ruido 
 def strToBinary(s):
     st = s
     return(' '.join(format(x, 'b') for x in bytearray(st, 'utf-8')))
@@ -57,9 +40,53 @@ def countTotalBits(num):
         else:
             binary = bin(int(a))[2:]
             TotalNumberBits = TotalNumberBits + len(binary)
-    print('Numero total de bits: ', TotalNumberBits)
-     
+    return TotalNumberBits
+
+#parte 2 - Algoritmo de deteccion y correcion
+#Algoritmo de Hamming
+def Calcular_Reduccion_Bits(m):
+    for i in range(m):
+        if(2**i >= m + i + 1):
+            return i
+
+def Redundancia_Bits(data, r): 
+    j = 0
+    k = 1
+    m = len(data) 
+    res = ''  
+    for i in range(1, m + r+1): 
+        if(i == 2**j): 
+            res = res + '0'
+            j += 1
+        else: 
+            res = res + data[-1 * k] 
+            k += 1
+    return res[::-1]
+
+def Calcular_Paridad_Bits(arr, r): 
+    n = len(arr) 
+    for i in range(r): 
+        val = 0
+        for j in range(1, n + 1):  
+            if(j & (2**i) == (2**i)): 
+                val = val ^ int(arr[-1 * j]) 
+        arr = arr[:n-(2**i)] + str(val) + arr[n-(2**i)+1:] 
+    return arr 
+
+def Detectar_Error(arr, nr): 
+    n = len(arr) 
+    res = 0
+    for i in range(nr):
+        val = 0
+        for j in range(1, n + 1):
+            if(j & (2**i) == (2**i)):
+                val = val ^ int(arr[-1 * j])
+        res = res + val*(10**i)
+    return int(str(res), 2) 
+
+# Parte Final - Poner todo a funcionar  
 final_message = []
+BinaryNumberString = ''
 key = True
 while key == True:
     MessageA = str(input("Ingrese un texto a enviar: "))
@@ -78,7 +105,22 @@ while key == True:
         print('Mensaje Invalido')
         key = True
         pass
+for u in BinaryNumber:
+    if u == ' ':
+        pass
+    else:
+        BinaryNumberString = BinaryNumberString + u
 
-countTotalBits(BinaryNumber)
-print(MessageA)
+TotalBits = countTotalBits(BinaryNumber)
+print('Numero de bits: ', TotalBits, '\n')
+print('Bitarray: ', MessageA, '\n')
+print('Numero binario: ', BinaryNumber, '\n')
+
+#Aplicar Hamming
+r = Calcular_Reduccion_Bits(TotalBits)
+arr = Redundancia_Bits(BinaryNumberString, r)
+arr = Calcular_Paridad_Bits(arr, r)
+print("La Data transferida es " + arr, '\n')
+correction = Detectar_Error(arr, r) 
+print("La posicion del error es " + str(correction))
 
